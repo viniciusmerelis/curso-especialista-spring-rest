621d546bae24083be04516859a29bee5fc3d0b69
vinicius <vinicius.merelis@gmail.com>
2022-05-31 22:22:53 -0300

    Adicionando anotacao Service

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
+@Service
 public class S3FotoStorageService implements FotoStorageService {
     @Override
     public void armazenar(NovaFoto novaFoto) {
