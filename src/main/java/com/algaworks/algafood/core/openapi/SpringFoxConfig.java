07bf889c7cade45d019f875c61bbabf72ab99a8c
vinicius <vinicius.merelis@gmail.com>
2022-09-19 21:12:23 -0300

    WIP - Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,7 +17,17 @@ public class SpringFoxConfig {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-//                .paths(PathSelectors.ant("/restaurantes/*"))
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cidades", "Gerencia as Cidades"));
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("AlgaFood", "https://www.algafood.com", "contato@algafood.com"))
                 .build();
     }
 }
