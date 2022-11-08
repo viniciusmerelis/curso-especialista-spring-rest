0f36d2b092809d9b10516123face4747fe6af577
vinicius <vinicius.merelis@gmail.com>
2022-11-07 21:25:30 -0300

    19.18. Adicionando links com template variables

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoAssemblerDTO.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoAssemblerDTO.java
@@ -10,6 +10,10 @@ import com.algaworks.algafood.api.model.PedidoDTO;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -28,9 +32,14 @@ public class PedidoAssemblerDTO extends RepresentationModelAssemblerSupport<Pedi
 
     @Override
     public PedidoDTO toModel(Pedido pedido) {
-        PedidoDTO pedidoDTO = createModelWithId(pedido.getId(), pedido);
+        PedidoDTO pedidoDTO = createModelWithId(pedido.getCodigo(), pedido);
         mapper.map(pedido, pedidoDTO);
-        pedidoDTO.add(linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM));
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+        pedidoDTO.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), "pedidos"));
         pedidoDTO.getRestaurante().add(linkTo(
                 methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())).withSelfRel());
         pedidoDTO.getCliente().add(linkTo(
