13647e7ee9405d06a22a050e9456922cf1361c19
vinicius <vinicius.merelis@gmail.com>
2023-01-11 15:25:02 -0300

    23.15. Desafio: refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/algaworks/algafood/domain/service/UsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,9 @@ public class UsuarioService {
 	
 	@Autowired 
 	private GrupoService grupoService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
@@ -28,17 +32,20 @@ public class UsuarioService {
 		if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
 			throw new NegocioException(
 					String.format("Já existe um usuário cadastrado com o e-mail: %s", usuario.getEmail()));
-		}		
+		}
+		if (usuario.isNovo()) {
+			usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		}
 		return usuarioRepository.save(usuario);
 	}
 	
 	@Transactional
 	public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
 		Usuario usuario = buscarOuFalhar(usuarioId);
-		if(usuario.senhaNaoCoincideCom(senhaAtual)) {
+		if(!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
 			throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
 		}
-		usuario.setSenha(novaSenha);
+		usuario.setSenha(passwordEncoder.encode(novaSenha));
 	}
 	
 	@Transactional
