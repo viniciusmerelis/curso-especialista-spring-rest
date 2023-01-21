723f23c2c9043b1140d3a15a0abc52a8beb2380c
vinicius <vinicius.merelis@gmail.com>
2023-01-21 20:00:52 -0300

    23.44. Customizando a página de OAuth2 Approval

--- src/main/java/com/algaworks/algafood/core/security/SecurityController.java
+++ src/main/java/com/algaworks/algafood/core/security/SecurityController.java
@@ -9,4 +9,9 @@ public class SecurityController {
     public String login() {
         return "pages/login";
     }
+
+    @GetMapping("/oauth/confirm_access")
+    public String approval() {
+        return "pages/approval";
+    }
 }
