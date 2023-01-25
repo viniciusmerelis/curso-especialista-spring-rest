0a3c3e7cba187e713a6b174143d76e2b74b5ffeb
vinicius <vinicius.merelis@gmail.com>
2023-01-25 13:13:20 -0300

    25.3. Dependência de JavaMailSender não satisfeita: melhorando o uso da herança. (Resolvendo problemas no Storage e Email)

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/FakeEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/FakeEnvioEmailService.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.infrastructure.service.email;
 
+import com.algaworks.algafood.domain.service.EnvioEmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService {
+public class FakeEnvioEmailService implements EnvioEmailService {
+	@Autowired
+	private ProcessadorTemplateEmail processadorTemplateEmail;
 	@Override
 	public void enviar(Mensagem mensagem) {
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorTemplateEmail.processarTemplate(mensagem);
 		log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
 	}
 }
