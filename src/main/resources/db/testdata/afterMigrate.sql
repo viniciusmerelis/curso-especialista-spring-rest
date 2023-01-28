05584524162e31453f422d5deb26ffd19cac98b0
vinicius <vinicius.merelis@gmail.com>
2023-01-28 17:59:44 -0300

    27.11. Implementando um cliente com o fluxo Authorization Code

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -181,21 +181,37 @@ insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, 
 values (6, 5, 3, 1, 87.2, 87.2, null);
 
 
-insert into oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types,
-                                  web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, autoapprove
+insert into oauth_client_details (
+    client_id, resource_ids, client_secret,
+    scope, authorized_grant_types, web_server_redirect_uri, authorities,
+    access_token_validity, refresh_token_validity, autoapprove
 )
-values ('algafood-web', null, '$2a$12$1kV5oIrp5LDEhIh9OF5gFuxoQFNXDA/d8jzojz5jbdTYvncVKUztG', 'READ,WRITE', 'password',
-        null, null, 60 * 60 * 6, 60 * 24 * 60 * 60, null);
+values (
+           'algafood-web', null, '$2y$12$w3igMjsfS5XoAYuowoH3C.54vRFWlcXSHLjX7MwF990Kc2KKKh72e',
+           'READ,WRITE', 'password,authorization_code', 'http://localhost:8080,http://localhost:8080/swagger-ui/oauth2-redirect.html', null,
+           60 * 60 * 6, 60 * 24 * 60 * 60, null
+       );
 
-insert into oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types,
-                                  web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, autoapprove
+insert into oauth_client_details (
+    client_id, resource_ids, client_secret,
+    scope, authorized_grant_types, web_server_redirect_uri, authorities,
+    access_token_validity, refresh_token_validity, autoapprove
 )
-values ('foodanalytics', null, '$2a$12$1kV5oIrp5LDEhIh9OF5gFuxoQFNXDA/d8jzojz5jbdTYvncVKUztG', 'READ,WRITE', 'authorization_code', 'http://localhost:8082', null, null, null, null);
+values (
+           'foodanalytics', null, '$2y$12$fahbH37S2pyk1RPuIHKP.earzFmgAJJGo26rE.59vf4wwiiTKHnzO',
+           'READ,WRITE', 'authorization_code', 'http://www.foodanalytics.local:8082', null,
+           null, null, null
+       );
 
-insert into oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types,
-                                  web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, autoapprove
+insert into oauth_client_details (
+    client_id, resource_ids, client_secret,
+    scope, authorized_grant_types, web_server_redirect_uri, authorities,
+    access_token_validity, refresh_token_validity, autoapprove
 )
-values ('faturamento', null, '$2a$12$1kV5oIrp5LDEhIh9OF5gFuxoQFNXDA/d8jzojz5jbdTYvncVKUztG','READ,WRITE', 'client_credentials',
-        null, 'CONSULTAR_PEDIDOS,GERAR_RELATORIOS',null, null, null);
+values (
+           'faturamento', null, '$2y$12$fHixriC7yXX/i1/CmpnGH.RFyK/l5YapLCFOEbIktONjE8ZDykSnu',
+           'READ,WRITE', 'client_credentials', null, 'CONSULTAR_PEDIDOS,GERAR_RELATORIOS',
+           null, null, null
+       );
 
 unlock tables;
